587faa5e1b4848c0c4cfa1c11e274b81a6f5e436
sycDev <sinyi.20028@gmail.com>
2025-01-01 12:41:26 +0800

    Fix scrollbar not displaying in kitchen page

--- RestaurantManagementSystem/View/kitchenViewForm.Designer.cs
+++ RestaurantManagementSystem/View/kitchenViewForm.Designer.cs
@@ -37,6 +37,7 @@
             this.mainFlowPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainFlowPanel.AutoScroll = true;
             this.mainFlowPanel.Location = new System.Drawing.Point(12, 12);
             this.mainFlowPanel.Name = "mainFlowPanel";
             this.mainFlowPanel.Size = new System.Drawing.Size(1176, 656);
